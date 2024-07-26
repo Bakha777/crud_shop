45b6f81a54502aa7862992052478054f754c6f26
Bakha777 <b.sharopov07@gmail.com>
2024-07-26 17:47:31 +0500

    Написали код для categories.

--- categories_api.py
+++ categories_api.py
@@ -3,11 +3,11 @@ import requests
 def get_all_categories():
     response = requests.get('https://fakestoreapi.com/products/categories')
     if response.status_code == 200:
-        products = response.json()
-        return products
+        categories = response.json()
+        return categories
 
-def get_specific_category(category_id):
-    response = requests.get(f'https://fakestoreapi.com/products/category/{category_id}')
+def get_specific_category(category_name):
+    response = requests.get(f'https://fakestoreapi.com/products/category/{category_name}')
     if response.status_code == 200:
         product = response.json()
-        return product
+        return products
